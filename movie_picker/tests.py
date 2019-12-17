d4ff5b53fda9a8bc77826ff2a33af394428c5004
Will Greene <greenew.k@gmail.com>
2019-12-16 20:00:17 -0500

    added Bootstrap, FT for ibox layout added but not passing

--- movie_picker/tests.py
+++ movie_picker/tests.py
@@ -1,13 +1,18 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from selenium import webdriver
 
-class HomePageTest(TestCase):
+class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
+class HomePageTest(FunctionalTest):
 
     # Jasper attempts to reach the Super Movie Picker website
     def test_can_reach_homepage(self):
@@ -15,6 +20,18 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     # Jasper sees that there is an input box for him to type movies into
+    # and it is nicely centered
+    def test_movie_title_input_box_present(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.get_item_input_box()
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
 
     # Jasper types a movie title in and hits Enter
 
